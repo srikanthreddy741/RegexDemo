cce0d4691c63ca54b54427649ddccb8cab2b5eba
Srikanth Reddy <bandarisrikanth741@gmail.com>
2023-01-14 08:48:04 +0530

    Add project files.

---
+++ RegexDemo/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace RegexDemo
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            //    Console.WriteLine(" enter the pincode");
+
+            //    string pin=Console.ReadLine();
+
+            //    bool val = Pincode.ValidatePincode(pin);
+
+            //    if(val)
+            //    {
+            //        Console.WriteLine("pincode is correct");
+
+            //    }
+            //    else
+            //    {
+            //        Console.WriteLine("pincode is incorrect");
+            //    }
+            //}
+            Console.WriteLine(" enter the phone number");
+
+            string pin = Console.ReadLine();
+
+            bool val = phonenumber.ValidatePhonenumber(pin);
+
+            if (val)
+            {
+                Console.WriteLine("phonenumber is correct");
+
+            }
+            else
+            {
+                Console.WriteLine("phonenumber is incorrect");
+            }
+        }
+    }
+}
---
+++ RegexDemo/phonenumber.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace RegexDemo
+{
+    public class phonenumber
+    {
+        public static string REGEX_PAT = "^[1-9]{1}[0-9]{9}$";
+
+        public static bool ValidatePhonenumber(string pat)
+        {
+            return Regex.IsMatch(pat , REGEX_PAT);
+        }
+    }
+}
